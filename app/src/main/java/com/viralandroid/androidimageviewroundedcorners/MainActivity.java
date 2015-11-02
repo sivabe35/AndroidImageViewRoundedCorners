d0597d94c181324f784152d3368d8f752ab39a4c
Prem Regmi <myfrozenpark@gmail.com>
2015-11-02 12:14:54 +0545

    Initial commit

---
+++ app/src/main/java/com/viralandroid/androidimageviewroundedcorners/MainActivity.java
@@ -0,0 +1,42 @@
+// How to Make an ImageView Image Rounded Corners in Android
+
+package com.viralandroid.androidimageviewroundedcorners;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapShader;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.graphics.Shader;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView textView = (TextView) findViewById(R.id.text_view);
+        textView.setText("ImageView Image Rounded Corners");
+        textView.setTextColor(Color.RED);
+        textView.setTextSize(20);
+
+        ImageView mimageView = (ImageView) findViewById(R.id.image_view);
+
+        Bitmap mbitmap = ((BitmapDrawable) getResources().getDrawable(R.drawable.cat)).getBitmap();
+        Bitmap imageRounded = Bitmap.createBitmap(mbitmap.getWidth(), mbitmap.getHeight(), mbitmap.getConfig());
+        Canvas canvas = new Canvas(imageRounded);
+        Paint mpaint = new Paint();
+        mpaint.setAntiAlias(true);
+        mpaint.setShader(new BitmapShader(mbitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP));
+        canvas.drawRoundRect((new RectF(0, 0, mbitmap.getWidth(), mbitmap.getHeight())), 100, 100, mpaint);// Round Image Corner 100 100 100 100
+        mimageView.setImageBitmap(imageRounded);
+    }
+
+}
